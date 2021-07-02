80f21743ac25dff251fa4da2ada361307850066f
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2021-07-02 18:55:52 +0200

    |

--- client/main.lua
+++ client/main.lua
@@ -5,7 +5,7 @@ local isRunningWorkaround = false
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
+		Citizen.Wait(1)
 	end
 end)
 
